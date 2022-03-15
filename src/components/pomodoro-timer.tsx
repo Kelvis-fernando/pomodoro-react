408e71617ff2f7240740b1f8526d22ed386ce06f
Kelvis Fernando <kelvisdelapena@gmail.com>
2022-03-15 07:44:51 -0300

    Added the functions to start work, and pause and play the timer, made the logic for change the color of the body, and turn the button of play and pause

--- src/components/pomodoro-timer.tsx
+++ src/components/pomodoro-timer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useInterval } from '../hooks/use-interval';
 import { Button } from './Button';
 import { Timer } from './Timer';
@@ -12,19 +12,36 @@ interface Props {
 
 export function PomodoroTimer(props: Props): JSX.Element {
   const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTimer);
+  const [timeWorking, setTimeWorking] = React.useState(false);
+  const [working, setWorking] = React.useState(false);
 
-  useInterval(() => {
-    setMainTime(mainTime - 1);
-  }, 1000);
+  useEffect(() => {
+    if (working) document.body.classList.add('working');
+  }, [working]);
+
+  useInterval(
+    () => {
+      setMainTime(mainTime - 1);
+    },
+    timeWorking ? 1000 : null,
+  );
+
+  const configWork = () => {
+    setTimeWorking(true);
+    setWorking(true);
+  };
 
   return (
     <div className="pomodoro">
       <h2>You Are: Working</h2>
       <Timer mainTime={mainTime} />
       <div className="controls">
+        <Button text="Work" onClick={() => configWork()} />
         <Button text="Start" />
-        <Button text="Start" />
-        <Button text="Start" />
+        <Button
+          text={timeWorking ? 'Pause' : 'Play'}
+          onClick={() => setTimeWorking(!timeWorking)}
+        />
       </div>
 
       <div className="details">
